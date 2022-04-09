12ea42968171bc5ddd6c368456267c3554b3aee6
Tobias Mbanusi <tobias.mba@gmail.com>
2022-04-09 01:18:07 +0100

    User Questions method 2

--- app/src/main/java/com/example/t_edits_app_tobias/teditsQuestions.java
+++ app/src/main/java/com/example/t_edits_app_tobias/teditsQuestions.java
@@ -4,20 +4,97 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class teditsQuestions extends AppCompatActivity {
     private FirebaseAuth mAuth;
+
+    private static final int REQUEST_CODF_IMAGE = 101;
+
+    private ImageView sketchViewAdd;
+
+    private EditText questionOne, questionTwo, questionThree;
+    private RadioGroup radioGroupOne, radioGroupTwoTwo, radioGroupTwoThree, radioGroupThreeOne, radioGroupFour;
+
+    Button submit;
+    Uri imageUri;
+
+    //Firebase Reference
+    private CollectionReference rFireStore;
+    private DatabaseReference Dataref;
+    private StorageReference Storageref;
+    private FirebaseStorage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tedits_questions_two);
+
+        sketchViewAdd = findViewById(R.id.sketchupload);
+
+        questionOne = findViewById(R.id.logonameText);
+        questionTwo = findViewById(R.id.targetAudience);
+        questionThree = findViewById(R.id.brandIndustry);
+
+        radioGroupOne = (RadioGroup) findViewById(R.id.optionone);
+        radioGroupTwoTwo = (RadioGroup) findViewById(R.id.optiontwotwo);
+        radioGroupTwoThree = (RadioGroup) findViewById(R.id.optiontwothree);
+        radioGroupThreeOne = (RadioGroup) findViewById(R.id.optionthree);
+        radioGroupFour = (RadioGroup) findViewById(R.id.optionfour);
+
+        submit = (Button) findViewById(R.id.submitOne);
+
+        Dataref = FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("LogoPackage");
+
+        sketchViewAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,REQUEST_CODF_IMAGE);
+
+            }
+        });
+        radioGroupOne.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup radioGroup, int checkId) {
+                        // Get the selected Radio Button
+                        RadioButton radioButton = (RadioButton)radioGroup.findViewById(checkId);
+                    }
+                }
+        );
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int selectedId = radioGroupOne.getCheckedRadioButtonId();
+                if(selectedId == -1) {
+                    Toast.makeText(teditsQuestions.this, "No option has been selected", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    RadioButton radioButton = (RadioButton) radioGroupOne.findViewById(selectedId);
+                    Toast.makeText(teditsQuestions.this, radioButton.getText(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -66,41 +143,7 @@ public class teditsQuestions extends AppCompatActivity {
                 if (checked)
                     break;
             case R.id.radio_teenager:
-                if (checked)
-                    break;
-            case R.id.radio_secondaryStudents:
-                if (checked)
-                    break;
-            case R.id.radio_collegeTeenager:
-                if (checked)
-                    break;
-            case R.id.radio_luxury:
-                if (checked)
-                    break;
-            case R.id.radio_sporty:
-                if (checked)
-                    break;
-            case R.id.radio_techwear:
-                if (checked)
-                    break;
-            case R.id.radio_Apparel:
-                if (checked)
-                    break;
-            case R.id.radio_cosmetics:
-                if (checked)
-                    break;
-            case R.id.radio_tech:
-                if (checked)
-                    break;
-            case R.id.radio_green:
-                if (checked)
-                    break;
-            case R.id.radio_blue:
-                if (checked)
-                    break;
-            case R.id.radio_orange:
-                if (checked)
-                    break;
+                if (checked);
         }
     }
 }
