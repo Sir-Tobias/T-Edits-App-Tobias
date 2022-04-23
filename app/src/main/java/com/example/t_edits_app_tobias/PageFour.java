3de81f085418d2b5b92d7525f9377a03ff6cf684
Tobias Mbanusi <tobias.mba@gmail.com>
2022-04-23 18:29:03 +0100

    Generating pallette progress

--- app/src/main/java/com/example/t_edits_app_tobias/PageFour.java
+++ app/src/main/java/com/example/t_edits_app_tobias/PageFour.java
@@ -66,6 +66,7 @@ public class PageFour extends AppCompatActivity {
 
     //SHARED PREFERENCES
     SharedPreferences sp;
+    SharedPreferences op;
 
 
     @Override
@@ -178,9 +179,9 @@ public class PageFour extends AppCompatActivity {
                 editor.commit();
                 Toast.makeText(PageFour.this,"Successfully saved question four to preferences", Toast.LENGTH_LONG).show();
 
-                if(primaryColour!=null) {
-                    submitFour(primaryColour, secondaryColour, neutralColour);
-                }
+//                if(primaryColour!=null) {
+//                    submitFour(primaryColour, secondaryColour, neutralColour);
+//                }
 
                 retrieveAnswers();
             }
@@ -191,100 +192,212 @@ public class PageFour extends AppCompatActivity {
 
     private void retrieveAnswers() {
         //STRING PACKAGE ANSWER
-        String packageAnswer;
+        String packageAnswer = null;
+
+        String answerOne = null;
+        String answerTwo = null;
+        String answerThree = null;
+        String answerFour = null;
+        String answerFive = null;
+        String answerSix = null;
+        String answerSeven = null;
+
 
         //LOADING THE SHARED PREFERENCES FROM PAGE ONE
         SharedPreferences sa = getApplicationContext().getSharedPreferences("AnswerOne", Context.MODE_PRIVATE);
         String nameLogo = sa.getString("nameLogo", "");
+        //Answer for the generator
         String typeLogo = sa.getString("typeLogo", "");
+
+        String imageUri = sa.getString("imageURI", "");
 
         //LOADING THE SHARED PREFERENCES FROM PAGE TWO
         SharedPreferences sb = getApplicationContext().getSharedPreferences("AnswerTwo", Context.MODE_PRIVATE);
         String targetAudience = sb.getString("targetAudience", "");
+        //Answer for the generator
         String genderAudience = sb.getString("genderAudience", "");
         String ageDemographic = sb.getString("ageDemographic", "");
 
         //LOADING THE SHARED PREFERENCES FROM PAGE THREE
         SharedPreferences sc = getApplicationContext().getSharedPreferences("AnswerThree", Context.MODE_PRIVATE);
         String industryDescription = sc.getString("industryDescription", "");
+        //Answer for the generator
         String industryType = sc.getString("industryType", "");
 
         //LOADING THE SHARED PREFERENCES FROM PAGE FOUR
         SharedPreferences sd = getApplicationContext().getSharedPreferences("AnswerFour", Context.MODE_PRIVATE);
+        //Answer for the generator
         String primaryColour = sd.getString("primaryColour", "");
+        //Answer for the generator
         String secondaryColour = sd.getString("secondaryColour", "");
+        //Answer for the generator
         String neutralColour = sd.getString("neutralColour", "");
 
-        if (nameLogo.isEmpty()) {
-            Toast.makeText(PageFour.this, "Logo name is required on page one", Toast.LENGTH_LONG).show();
-        }
         //ANSWER 1.1 WHAT IS YOUR LOGO TYPE?
-        if (typeLogo.equalsIgnoreCase("Icon type")) {
-        }
-        else if (typeLogo.equalsIgnoreCase("Name type")) {
-        }
+        HashMap<String, String>lType = new HashMap<>();
+        lType.put("Icon type", "AA,");
+        lType.put("Name type", "AB,");
+        //SETTING ANSWERS TO THE SHAREDPREFERENCE VALUE WHICH WILL CONVERT IT INTO A CODE VALUE
+        answerOne = lType.get(typeLogo);
+
 
         //ANSWER 2.1 WHAT IS THE GENDER AUDIENCE OF YOUR BRAND?
-        if (genderAudience.equalsIgnoreCase("Male")) {
-        }
-        else if (genderAudience.equalsIgnoreCase("Female")) {
-        }
-        else if (genderAudience.equalsIgnoreCase("Gender Neutral")) {
-        }
+        HashMap<String, String>gAudience = new HashMap<>();
+        gAudience.put("Male", "BA,");
+        gAudience.put("Female", "BB,");
+        gAudience.put("Gender Neutral", "BC,");
+
+        answerTwo = answerOne + gAudience.get(genderAudience);
+
 
         //ANSWER 2.2 WHAT IS THE AGE DEMOGRAPHIC FOR YOUR BRAND?
-        if (ageDemographic.equalsIgnoreCase("15 >")) {
-        }
-        else if (ageDemographic.equalsIgnoreCase("15 - 45")) {
-        }
-        else if (ageDemographic.equalsIgnoreCase("45 +")) {
-        }
+        HashMap<String, String>aDemographic = new HashMap<>();
+        aDemographic.put("15 >", "BD,");
+        aDemographic.put("15 - 45", "BE,");
+        aDemographic.put("45 +", "BF,");
+
+        answerThree = answerTwo + aDemographic.get(ageDemographic);
+
 
         //ANSWER 3.1 WHAT IS THE INDUSTRY OF YOUR BRAND?
-        if (industryType.equalsIgnoreCase("Apparel")) {
-        }
-        else if (industryType.equalsIgnoreCase("Media")) {
-        }
-        else if (industryType.equalsIgnoreCase("Cosmetics")) {
-        }
-        else if (industryType.equalsIgnoreCase("Tech")) {
-        }
+        HashMap<String, String>bIndustry = new HashMap<>();
+        bIndustry.put("Apparel", "CA,");
+        bIndustry.put("Media", "CB,");
+        bIndustry.put("Cosmetics", "CC,");
+        bIndustry.put("Tech", "CD,");
+
+        answerFour = answerThree + bIndustry.get(industryType);
 
         //ANSWER 4.0 ENTER YOUR PRIMARY COLOUR?
-        if (primaryColour.equalsIgnoreCase("White")) {
-        }
-        else if (primaryColour.equalsIgnoreCase("Yellow")) {
-        }
-        else if (primaryColour.equalsIgnoreCase("Red")) {
-        }
-        else if (primaryColour.equalsIgnoreCase("Blue")) {
-        }
-        else if (primaryColour.equalsIgnoreCase("Black")) {
-        }
+        HashMap<String, String>pColours = new HashMap<>();
+        pColours.put("White", "DA,");
+        pColours.put("Yellow", "DB,");
+        pColours.put("Red", "DC,");
+        pColours.put("Blue", "DD,");
+        pColours.put("Black", "DE,");
 
-        //ANSWER 4.1 ENTER YOUR SECONDARY COLOUR?
-        if (primaryColour.equalsIgnoreCase("Orange")) {
-        }
-        else if (secondaryColour.equalsIgnoreCase("Green")) {
-        }
-        else if (secondaryColour.equalsIgnoreCase("Purple")) {
-        }
+        answerFive = answerFour + pColours.get(primaryColour);
 
-        //ANSWER 4.2 ENTER YOUR NEUTRAL COLOUR?
-        if (primaryColour.equalsIgnoreCase("Brown")) {
-        }
-        else if (secondaryColour.equalsIgnoreCase("Beige")) {
-        }
-        else if (secondaryColour.equalsIgnoreCase("Grey")) {
-        }
+        //ANSWER 4.2 ENTER YOUR SECONDARY COLOUR?
+        HashMap<String, String>sColours = new HashMap<>();
+        sColours.put("Orange", "DF,");
+        sColours.put("Green", "DG,");
+        sColours.put("Purple", "DH,");
 
+        answerSix = answerFive + pColours.get(secondaryColour);
+
+        //ANSWER 4.3 ENTER YOUR NEUTRAL COLOUR?
+        HashMap<String, String>nColours = new HashMap<>();
+        nColours.put("Brown", "DJ");
+        nColours.put("Beige", "DK");
+        nColours.put("Grey", "DL");
+
+        answerSeven = answerSix + nColours.get(neutralColour);
+
+        packageAnswer = answerSeven;
+
+        //PASSING THE PACKAGE ANSWER STRING INTO A SHAREDPREFERENCE
+        SharedPreferences.Editor oEditor = op.edit();
+
+        oEditor.putString("packageAnswer", packageAnswer);
+        oEditor.commit();
+        Toast.makeText(PageFour.this,"Successfully saved packageAnswer", Toast.LENGTH_LONG).show();
+
+        submitFour(nameLogo, typeLogo, imageUri, targetAudience, genderAudience , ageDemographic, industryDescription, industryType, primaryColour, secondaryColour, neutralColour);
+
+
+//        if (nameLogo.isEmpty()) {
+//            Toast.makeText(PageFour.this, "Logo name is required on page one", Toast.LENGTH_LONG).show();
+//        }
+//        //ANSWER 1.1 WHAT IS YOUR LOGO TYPE?
+//        if (typeLogo.equalsIgnoreCase("Icon type")) {
+//        }
+//        else if (typeLogo.equalsIgnoreCase("Name type")) {
+//        }
+
+
+
+
+        //ANSWER 2.1 WHAT IS THE GENDER AUDIENCE OF YOUR BRAND?
+//        if (genderAudience.equalsIgnoreCase("Male")) {
+//        }
+//        else if (genderAudience.equalsIgnoreCase("Female")) {
+//        }
+//        else if (genderAudience.equalsIgnoreCase("Gender Neutral")) {
+//        }
+
+
+
+        //ANSWER 2.2 WHAT IS THE AGE DEMOGRAPHIC FOR YOUR BRAND?
+//        if (ageDemographic.equalsIgnoreCase("15 >")) {
+//        }
+//        else if (ageDemographic.equalsIgnoreCase("15 - 45")) {
+//        }
+//        else if (ageDemographic.equalsIgnoreCase("45 +")) {
+//        }
+
+
+
+        //ANSWER 3.1 WHAT IS THE INDUSTRY OF YOUR BRAND?
+//        if (industryType.equalsIgnoreCase("Apparel")) {
+//        }
+//        else if (industryType.equalsIgnoreCase("Media")) {
+//        }
+//        else if (industryType.equalsIgnoreCase("Cosmetics")) {
+//        }
+//        else if (industryType.equalsIgnoreCase("Tech")) {
+//        }
+
+
+
+
+//        //ANSWER 4.0 ENTER YOUR PRIMARY COLOUR?
+//        if (primaryColour.equalsIgnoreCase("White")) {
+//        }
+//        else if (primaryColour.equalsIgnoreCase("Yellow")) {
+//        }
+//        else if (primaryColour.equalsIgnoreCase("Red")) {
+//        }
+//        else if (primaryColour.equalsIgnoreCase("Blue")) {
+//        }
+//        else if (primaryColour.equalsIgnoreCase("Black")) {
+//        }
+
+
+//        if (primaryColour.equalsIgnoreCase("Orange")) {
+//        }
+//        else if (secondaryColour.equalsIgnoreCase("Green")) {
+//        }
+//        else if (secondaryColour.equalsIgnoreCase("Purple")) {
+//        }
+
+
+        //ANSWER 4.3 ENTER YOUR NEUTRAL COLOUR?
+//        if (primaryColour.equalsIgnoreCase("Brown")) {
+//        }
+//        else if (secondaryColour.equalsIgnoreCase("Beige")) {
+//        }
+//        else if (secondaryColour.equalsIgnoreCase("Grey")) {
+//        }
+//
     }
 
     //ANSWER PAGE THREE
-    private void submitFour(String primaryColour, String secondaryColour, String neutralColour) {
+    private void submitFour(String nameLogo, String typeLogo, String imageUri, String targetAudience, String genderAudience, String ageDemographic, String industryDescription, String industryType, String primaryColour, String secondaryColour, String neutralColour) {
         final String key = Dataref.push().getKey();
 
         HashMap hashMap = new HashMap();
+        hashMap.put("1 Name of logo: ", nameLogo);
+        hashMap.put("1*2 Type of logo: ", typeLogo);
+        hashMap.put("1*3 Sketch of logo: ", imageUri);
+
+        hashMap.put("2* Description of brand: ", targetAudience);
+        hashMap.put("2*1 Type of Gender audience: ", genderAudience);
+        hashMap.put("2*2 Age demographic: ", ageDemographic);
+
+        hashMap.put("3* Description of brand industry: ", industryDescription);
+        hashMap.put("3*1 Type of Gender audience: ", industryType);
+
         hashMap.put("4* Select a primary colour: ", primaryColour);
         hashMap.put("4*1 Select a secondary colour: ", secondaryColour);
         hashMap.put("4*2 Select a neutral colour: ", neutralColour);
@@ -293,7 +406,7 @@ public class PageFour extends AppCompatActivity {
             @Override
             public void onSuccess(Void unused) {
 
-                Toast.makeText(PageFour.this,"Question four has been submitted", Toast.LENGTH_LONG).show();
+                Toast.makeText(PageFour.this,"All answers have been submitted has been submitted", Toast.LENGTH_LONG).show();
                 //setContentView(R.layout.activity_page_four);
             }
         }).addOnFailureListener(new OnFailureListener() {
